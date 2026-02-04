365202e9da28dbaf112251f0a15e70d04245bd71
taylor <tailorleaf@gmail.com>
2026-02-03 22:24:55 -0800

    write callback function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 #include <curl/curl.h>
 
-size_t write_callback(char *ptr, size_t size, size_t nmemb,
-                      void *userdata);
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata){
+    std::string *buffer = (std::string*) userdata;
+    buffer->append(ptr, size * nmemb)
+    return size * nmemb;//return the number of bytes actually taken care of
+}
 
 int main(){
     return 0;
